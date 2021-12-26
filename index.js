d1a117e68f59d1f43b0126afed49caae5ed8a942
Mark Teekman <markteekman@users.noreply.github.com>
2021-12-26 08:49:31 +0100

    Add Modal component

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 export { default as Accordion } from './Accordion.astro'
 export { default as AccordionItem } from './AccordionItem.astro'
 export { default as Card } from './Card.astro'
+export { default as Modal } from './Modal.astro'
