42fa924b2e2c9ce5fe0f9187e33304b05d771594
Mark Teekman <markteekman@users.noreply.github.com>
2023-04-17 20:40:16 +0200

    Small spellchecks

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,20 +1,20 @@
 /**
- * Accordion Item child component
+ * Accordion item child component
  *
  * @param _props - Record<string, any>
- * @param _props.header - `<H3>` header text content
- * @param _props.children - any HTML element/s, Parent element: `<div>`
+ * @param _props.header - `<h3>` header text content
+ * @param _props.children - Any HTML elements. Parent element: `<div>`
  */
 export type AccordionItem = typeof import('./AccordionItem.astro').default
 export const AccordionItem: AccordionItem
 
 /**
- * Accordion Parent component
+ * Accordion parent component
  *
  * @param _props - Record<string, any>
  * @param _props.children -
- *  - Expects one or more AccordionItem components, Parent Element: `<ul>`
- *  - WARNING: Astro cannot currently enforce the type of children in a Slot)
+ *  - Expects one or more AccordionItem components. Parent element: `<ul>`
+ *  - WARNING: Astro cannot currently enforce the type of children in a `<slot>`
  */
 type Accordion = typeof import('./Accordion.astro').default
 export const Accordion: Accordion
@@ -31,12 +31,12 @@ export type BreadcrumbsItem = typeof import('./BreadcrumbsItem.astro').default
 export const BreadcrumbsItem: BreadcrumbsItem
 
 /**
- * Breadcrumbs Parent component
+ * Breadcrumbs parent component
  *
  * @param _props - Record<string, any>
  * @param _props.children -
- *  - Expects one or more BreadcrumbsItem components, Parent Element: `<ol>`
- *  - WARNING: Astro cannot currently enforce the type of children in a Slot)
+ *  - Expects one or more BreadcrumbsItem components. Parent element: `<ol>`
+ *  - WARNING: Astro cannot currently enforce the type of children in a `<slot>`
  */
 type Breadcrumbs = typeof import('./Breadcrumbs.astro').default
 export const Breadcrumbs: Breadcrumbs
@@ -47,16 +47,16 @@ export const Breadcrumbs: Breadcrumbs
  * @param _props - Record<string, any>
  * @param _props.url - `<a href={url}>` - default: "#"
  * @param _props.img - `<img src={img}>` - default value = placeholder
- * @param _props.title - `<H3>` header > `<a>` text content
+ * @param _props.title - `<h3>` header > `<a>` text content
  * @param _props.footer - `<small>` text content
- * @param _props.children - textContent or any legal P tag innerHtml such as inline HTML elements, Parent Element: `<p>`
+ * @param _props.children - textContent or any legal `<p>` tag innerHTML such as inline HTML elements. Parent element: `<p>`
  */
 type Card = typeof import('./Card.astro').default
 export const Card: Card
 
 /**
- * DorkMode toggle component
- * - toggles class `darkmode` on `document.body`
+ * DarkMode toggle component
+ * - Toggles class `darkmode` on `document.body`
 *
 * @param _props - Record<string, never>
 * ```
@@ -74,7 +74,7 @@ export const DarkMode: DarkMode
  * Media component
  *
  * @param _props - Record<string, any>
- * @param _props.class - string of one or more css class names
+ * @param _props.class - String of one or more CSS class names
  * @param _props.src - `<img src={src}>` - default: placeholder
  * @param _props.alt - `<img alt={alt}>` required for non-decorative images
  */
@@ -85,9 +85,9 @@ export const Media: Media
  * Modal component
  *
  * @param _props - Record<string, any>
- * @param _props.title - `<H3>` header text content
+ * @param _props.title - `<h3>` header text content
  * @param _props.closeText - `<button>` text content - default: "Close"
- * @param _props.children - any HTML element/s, Parent element: `<div>`
+ * @param _props.children - Any HTML elements. Parent element: `<div>`
  */
 type Modal = typeof import('./Modal.astro').default
 export const Modal: Modal
@@ -96,13 +96,13 @@ export const Modal: Modal
  * Notification component
  *
  * @param _props - Record<string, any>
- * @param _props.type - Specifies background colore: info = blue, success = green, warning = yellow, error = red - default: browser default color
- * @param _props.role - type of aria role: 'alert' | 'log' | 'marquee' | 'status' | 'timer' -  default: 'none'
+ * @param _props.type - Specifies background color: info = blue, success = green, warning = yellow, error = red - default: browser default color
+ * @param _props.role - Type of aria role: 'alert' | 'log' | 'marquee' | 'status' | 'timer' -  default: 'none'
  * @param _props.ariaLive -
  * - Defines urgency of live announcements, when used prefer 'polite'
  * - Warning: Because an interruption may disorient users or cause them to not complete their current task,
  *   don't use the assertive value unless the interruption is imperative.
- * @param _props.children - any HTML element/s, Parent element: `<div>`
+ * @param _props.children - any HTML elements. Parent element: `<div>`
  */
 type Notification = typeof import('./Notification.astro').default
 export const Notification: Notification
